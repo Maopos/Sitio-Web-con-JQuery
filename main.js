7aa2bafd9a65cfd0914c0cf0e419331be87757a9
Maopos <mauricioposadamurillo@gmail.com>
2021-01-24 15:44:06 -0500

    Versión 21.01.24

---
+++ main.js
@@ -0,0 +1,62 @@
+
+    $(document).ready(function(){
+
+        //slider
+      $('.slider').bxSlider({
+          mode: 'fade',
+          captions: true, //Subtitutlo debajo de la foto
+          slideWidth: 1200 //https://bxslider.com/options/
+          
+      });
+
+      //Post
+
+      var posts = [
+          {
+              title: 'Titulo articulo 1',
+              date: 'Publicado el día ' + moment().format("DD") + ' de ' + moment().format('MMMM, YYYY.'),
+              content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus rem optio necessitatibus blanditiis facilis, vitae sit iste earum quaerat explicabo sint vel, odio porro autem alias nostrum numquam ipsam magni. Mollitia maxime optio ea atque necessitatibus quo beatae officia? Illo vero unde, reiciendis error excepturi, beatae assumenda iure dignissimos tempore non doloribus quibusdam ipsam, necessitatibus fuga earum ad consequuntur nesciunt soluta libero.'
+          },
+          {
+            title: 'Titulo articulo 2',
+            date: 'Publicado el día ' + moment().format("DD") + ' de ' + moment().format('MMMM, YYYY.'),
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus rem optio necessitatibus blanditiis facilis, vitae sit iste earum quaerat explicabo sint vel, odio porro autem alias nostrum numquam ipsam magni. Mollitia maxime optio ea atque necessitatibus quo beatae officia? Illo vero unde, reiciendis error excepturi, beatae assumenda iure dignissimos tempore non doloribus quibusdam ipsam, necessitatibus fuga earum ad consequuntur nesciunt soluta libero.'
+          },
+          {
+            title: 'Titulo articulo 3',
+            date: 'Publicado el día ' + moment().format("DD") + ' de ' + moment().format('MMMM, YYYY.'),
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus rem optio necessitatibus blanditiis facilis, vitae sit iste earum quaerat explicabo sint vel, odio porro autem alias nostrum numquam ipsam magni. Mollitia maxime optio ea atque necessitatibus quo beatae officia? Illo vero unde, reiciendis error excepturi, beatae assumenda iure dignissimos tempore non doloribus quibusdam ipsam, necessitatibus fuga earum ad consequuntur nesciunt soluta libero.'
+          },
+          {
+            title: 'Titulo articulo 4',
+            date: 'Publicado el día ' + moment().format("DD") + ' de ' + moment().format('MMMM, YYYY.'),
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus rem optio necessitatibus blanditiis facilis, vitae sit iste earum quaerat explicabo sint vel, odio porro autem alias nostrum numquam ipsam magni. Mollitia maxime optio ea atque necessitatibus quo beatae officia? Illo vero unde, reiciendis error excepturi, beatae assumenda iure dignissimos tempore non doloribus quibusdam ipsam, necessitatibus fuga earum ad consequuntur nesciunt soluta libero.'
+          },
+          {
+            title: 'Titulo articulo 5',
+            date: 'Publicado el día ' + moment().format("DD") + ' de ' + moment().format('MMMM, YYYY.'),
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus rem optio necessitatibus blanditiis facilis, vitae sit iste earum quaerat explicabo sint vel, odio porro autem alias nostrum numquam ipsam magni. Mollitia maxime optio ea atque necessitatibus quo beatae officia? Illo vero unde, reiciendis error excepturi, beatae assumenda iure dignissimos tempore non doloribus quibusdam ipsam, necessitatibus fuga earum ad consequuntur nesciunt soluta libero.'
+          }
+          
+      ];
+
+      posts.forEach((item, index) => {
+          var post = `
+                            <article class="post">
+                                <h2>${item.title}</h2>
+                                <p id="fecha">${item.date}</p>
+                                <p>${item.content}</p>
+                                <a href="#" class="leer_mas">Leer mas...</a>
+                                <br><br>
+                                    <hr>
+                            </article> 
+          `;
+
+          $('#articulos').append(post);
+      });
+        
+        
+        
+
+    });
+  
